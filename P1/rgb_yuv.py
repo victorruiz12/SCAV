852780445846ebe962e25720ddb6a0f6b6ea6466
Victor Ruiz <victorruiz@MacBook-Air-de-Victor.local>
2020-11-18 12:11:43 +0100

    Última versión P1

--- P1/rgb_yuv.py
+++ P1/rgb_yuv.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy
-#prueba git
+
+
 
 # open images and make the same size
 im = Image.open("eat_micro.jpg").resize((256, 256))
@@ -30,15 +31,31 @@ print ("The value R after YCbCr->RGB is ",R)
 
 print("Close enough!")
 
+#cv2.COLOR_BGR2YUV
+#cv2.COLOR_BGR2YCrCb
 
 
+# Para pasar a YUV:
+
+#import cv2
+
+## get all color codes
+
+#codes = [x for x in dir(cv2) if x.startswith("COLOR_")]
+
+## print first three color codes
+#print(codes[:3])
+
+# ['COLOR_BAYER_BG2BGR', 'COLOR_BAYER_BG2BGRA', 'COLOR_BAYER_BG2BGR_EA']
+
+# print all color codes
+#print(codes)
+
+#img = cv2.imread("eat_micro.jpg")
+#yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+#cv2.imwrite("yuv.png", yuv)
 
 
-
-
-#16. + (65.481 * R + 128.553 * G + 24.966 * B) / 255.
-
-#ycbcr2 = im.merge('YCbCr', (y, cb, cr))
 
 
 
